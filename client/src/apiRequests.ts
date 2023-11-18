e5184a4b8c37c184ac075922e054144098f48f27
alanaherlands <alana@herlands.com>
2023-11-18 10:25:52 -0500

    Initial commit

--- client/src/apiRequests.ts
+++ client/src/apiRequests.ts
@@ -1,5 +1,14 @@
 // Get request - get access token
+const getAccessToken = async() => {
+
+}
 
 // Get request - get patient ID
+const getPatientID = async() => {
+
+}
 
 // Get request - get ExplanationOfBenefit
+const getExplanationOfBenefit = async() => {
+    
+}
---
+++ client/src/index.ts
@@ -0,0 +1,2 @@
+import { useEffect, useState } from 'react';
+import eobDisplay from './components/eobDisplay';
