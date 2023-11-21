a58e6fcf69548558bb16730283d3d265aabca6a9
alanaherlands <alana@herlands.com>
2023-11-20 20:16:20 -0500

    Created first version of the introspectToken file, created flexpaService file

--- client/src/services/exchangeToken.ts
+++ client/src/services/exchangeToken.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
-import { fetchDataFromFlexpa } from '../../../services/flexpaService';
-import { ApiResponse } from '../../../types';
+import { fetchDataFromFlexpa } from './flexpaService';
+import { ApiResponse } from '../types';
 
 export default async function handler(
   req: NextApiRequest,
