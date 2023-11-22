c3b4f88d601894f71d9c00e30b676dc43b9473c9
alanaherlands <alana@herlands.com>
2023-11-21 20:31:47 -0500

    Added module to Home.css and fixed issue. Renamed app to _app in accordance with Next.js docs to try to address app rendering issue.

---
+++ client/services/EoB.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { ApiResponse } from '../types';
+
+const eobHandler = async (req: NextApiRequest, res: NextApiResponse<ApiResponse>) => {
+    try {
+        // validate method
+        if (req.method !== 'GET') {
+            res.status(400).json({ data: 'Invalid request method' });
+            return;
+        }
+
+        // extract access token and patient ID
+        const accessToken = req.headers['access-token'];
+        const { patient: patientId } = req.query;
+
+        // make request to Flexpa API endpoint
+        const requestUrl = `https://api.flexpa.com/fhir/ExplanationOfBenefit?patient=${patientId}`;
+        const flexpaApiResponse = await fetch(requestUrl, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`,
+                'X-Flexpa-Raw': 'true'
+            },
+        });
+
+        // set HTTP status based on Flexpa API response
+        res.status(flexpaApiResponse.status);
+
+        if (flexpaApiResponse.status !== 200) {
+            // error from Flexpa API
+            res.json({ data: flexpaApiResponse.statusText });
+        } else {
+            // success from Flexpa API
+            const flexpaApiData = await flexpaApiResponse.json();
+            res.json({ data: flexpaApiData });
+        }
+    } catch (error: any) {
+        console.error(error.message);
+        res.status(500).json({ data: 'Error parsing JSON in API response' });
+    }
+};
+
+export default eobHandler;
---
+++ client/services/introspectToken.ts
@@ -0,0 +1,71 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { ApiResponse } from '../types';
+
+// handle the Flexpa API request
+const makeFlexpaRequest = async (url: string, options: RequestInit): Promise<Response> => {
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(`Failed to make Flexpa API request: ${response.statusText}`);
+    }
+
+    return response;
+  } catch (error: any) {
+    throw new Error(`Error making Flexpa API request: ${error.message}`);
+  }
+};
+
+// parse JSON or handle errors
+const parseJsonOrError = async (response: Response): Promise<Record<string, unknown>> => {
+  try {
+    return await response.json();
+  } catch (error: any) {
+    throw new Error(`Error parsing JSON in API response: ${error.message}`);
+  }
+};
+
+const handler = async(req: NextApiRequest, res: NextApiResponse<ApiResponse>) => {
+  try {
+    // validate request method
+    if (req.method !== 'POST') {
+      res.status(400).json({ data: 'Invalid request method' });
+      return;
+    }
+
+    // extract access token from request headers
+    const accessToken = req.headers['access-token'];
+
+    // make request to Flexpa API endpoint
+    const flexpaResponse = await makeFlexpaRequest(
+      'https://api.flexpa.com/link/introspect',
+      {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    // set HTTP status based on Flexpa API response
+    res.status(flexpaResponse.status);
+
+    // handle response data or errors
+    try {
+      const responseData = await parseJsonOrError(flexpaResponse);
+
+      // success parsing JSON
+      res.json({ data: responseData });
+    } catch (error: any) {
+      // error parsing JSON
+      res.status(500).json({ data: error.message });
+    }
+  } catch (error: any) {
+    // handle general errors
+    console.error(error.message);
+    res.status(500).json({ data: 'Internal Server Error' });
+  }
+}
+
+export { makeFlexpaRequest, parseJsonOrError, handler };
